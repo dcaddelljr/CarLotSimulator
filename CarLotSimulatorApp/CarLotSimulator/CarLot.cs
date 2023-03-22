a48d0e94ab58d54e81c1126d8eee784d57726ef5
Dwight Caddell <dcaddelljr@gmail.com>
2023-03-21 19:30:17 -0500

    CarLotSimulator2ndTime

--- CarLotSimulatorApp/CarLotSimulator/CarLot.cs
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -11,8 +11,18 @@ namespace CarLotSimulator
 		public CarLot()
 		{ }
 
-			public List<Car> ParkingLot { get; set; } = new List<Car>();
-			
+		//Have this number only increment when in our program we create a new Car.
+		//From there create 3 cars in the Main() and then each time you create a car, do a Console.WriteLine() printing the current number of cars in the car lot.
+		public static int numberOfCars = 1;
+
+
+		public List<Car> ParkingLot { get; set; } = new List<Car>();
+
+		
 	}
+        
+
+		
+	
 }
 
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -40,29 +40,33 @@ namespace CarLotSimulator
                 var car3 = new Car("1988", "Chevy", "Suburban", "VROOM", "Honk", false);
 
                 dwightsCarLot.ParkingLot.Add(car3);
-                //Create a seperate class file called Car
-                //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-                //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-                //The methods should take one string parameter: the respective noise property
+            //Create a seperate class file called Car
+            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+            //The methods should take one string parameter: the respective noise property
 
 
-                //Now that the Car class is created we can instanciate 3 new cars
-                //Set the properties for each of the cars
-                //Call each of the methods for each car
+            //Now that the Car class is created we can instanciate 3 new cars
+            //Set the properties for each of the cars
+            //Call each of the methods for each car
 
-                //*************BONUS*************//
+            //*************BONUS*************//
 
-                // Set the properties utilizing the 3 different ways we learned about, one way for each car
+            // Set the properties utilizing the 3 different ways we learned about, one way for each car
 
 
 
-                //*************BONUS X 2*************//
+            //*************BONUS X 2*************//
 
-                //Create a CarLot class
-                //It should have at least one property: a List of cars
-                //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
-                //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+            //Create a CarLot class
+            //It should have at least one property: a List of cars
+            //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
+            //At the end iterate through the list printing each of car's Year, Make, and Model to the console
 
+
+            Console.WriteLine($"Number of cars created: {CarLot.numberOfCars}");
+
+            
                 foreach (var car in dwightsCarLot.ParkingLot)
                 {
                     Console.WriteLine($"Year: {car.Year}");
